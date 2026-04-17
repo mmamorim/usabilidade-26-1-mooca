75d2a1920b064cba7eddc3e480168284b3e8cb2d
Marcelo <marc.m.amorim@gmail.com>
2026-04-16 21:44:13 -0300

    v1

--- Aula09_16ABR/main.js
+++ Aula09_16ABR/main.js
@@ -1,2 +1,42 @@
 
 console.log("oi gente")
+
+let listaFrutas = [
+    { avatar: "🍉", nome: "Melancia", cor: "vermelha" },
+    { avatar: "🍇", nome: "Uva", cor: "roxa" },
+    { avatar: "🍇", nome: "Uva2", cor: "roxa" },
+    { avatar: "🍇", nome: "Uva3", cor: "roxa" },
+]
+
+for (let i in listaFrutas) {
+    insereLinha(listaFrutas[i])
+}
+
+function acaoAdicionar() {
+    let avatar = document.getElementById("avatar").value
+    let nome = document.getElementById("nome").value
+    let cor = document.getElementById("cor").value
+
+    let fruta = {}
+    fruta.avatar = avatar
+    fruta.nome = nome
+    fruta.cor = cor
+    console.log(fruta)
+    insereLinha(fruta)
+    //console.log("Avatar", avatar)
+    //console.log("Nome", nome)
+    //console.log("Cor", cor)
+
+}
+
+function insereLinha(fruta) {
+    let elem = document.getElementById("lista")
+    let htmlString = `
+        <tr>
+            <td>`+ fruta.avatar + `</td>
+            <td>`+ fruta.nome + `</td>
+            <td>`+ fruta.cor + `</td>
+        </tr>    
+    `
+    elem.insertAdjacentHTML('beforeend', htmlString)
+}
